61a13ac76b0285f5b091fb59623d3f650b6d9f9e
Otabek <Otabek>
2024-03-30 15:10:54 +0500

    rwseg

---
+++ script.js
@@ -0,0 +1,11 @@
+let str = +prompt("!!!hi!", "!!!hi!")
+let num = +prompt("!!!hi!", "Undovlarni o'chirish uchun son kriting")
+if (num == 1 || num == 0) {
+    console.log("!!hi!");
+}else if (num == 2) {
+    console.log("!hi!");
+}else if (num == 3) {
+    console.log("hi!");
+}else{
+    console.log("hi");
+}
